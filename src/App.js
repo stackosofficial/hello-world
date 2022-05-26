9fefdefb562bf2597d525edf1a4cab4c183f4ee5
developerdesigner18 <developer.designer18@gmail.com>
2022-05-26 18:16:23 +0530

    test25

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Hello World | DEPLOYMENT
+          Hello World
         </a>
       </header>
     </div>
