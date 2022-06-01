436ab713a62ec6c5eae4310044449da684ee384a
developerdesigner18 <developer.designer18@gmail.com>
2022-06-01 16:35:50 +0530

    test be

--- backend/src/App.js
+++ backend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Backend Project Test
+          Backend Project
         </a>
       </header>
     </div>
